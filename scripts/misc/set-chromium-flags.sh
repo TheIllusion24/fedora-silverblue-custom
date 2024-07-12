1edd654a07d29fe4c51b6ff12661b82c99f4dba8
TheIllusion24 <ggfekarc@gmail.com>
2024-07-13 05:11:26 +0800

    feat: set ozone-platform-hint to auto

--- scripts/misc/set-chromium-flags.sh
+++ scripts/misc/set-chromium-flags.sh
@@ -9,6 +9,6 @@ sed -i 's/FEATURES=""/FEATURES="WebRTCPipeWireCapturer,TouchpadOverscrollHistory
 
 echo '
 
-CHROMIUM_FLAGS+=" --ozone-platform=wayland --no-pings --disk-cache-dir=/dev/null --extension-content-verification=enforce_strict --extensions-install-verification=enforce_strict --disable-features=PrivacySandboxSettings4,InterestFeedV2,NTPPopularSitesBakedInContent,UsePopularSitesSuggestions,MediaDrmPreprovisioning,AutofillServerCommunication,DisableThirdPartyStoragePartitioningDeprecationTrial,OptimizationHints,OptimizationHintsFetching,OptimizationHintsFetchingAnonymousDataConsent --enable-gpu-rasterization --enable-zero-copy --video-capture-use-gpu-memory-buffer"
+CHROMIUM_FLAGS+=" --ozone-platform-hint=auto --no-pings --disk-cache-dir=/dev/null --extension-content-verification=enforce_strict --extensions-install-verification=enforce_strict --disable-features=PrivacySandboxSettings4,InterestFeedV2,NTPPopularSitesBakedInContent,UsePopularSitesSuggestions,MediaDrmPreprovisioning,AutofillServerCommunication,DisableThirdPartyStoragePartitioningDeprecationTrial,OptimizationHints,OptimizationHintsFetching,OptimizationHintsFetchingAnonymousDataConsent --enable-gpu-rasterization --enable-zero-copy --video-capture-use-gpu-memory-buffer"
 
 ' >> /etc/chromium/chromium.conf
