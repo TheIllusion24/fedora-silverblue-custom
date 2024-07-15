7ddca35ce1e2eb59191fb9935b7de4115495b6ea
TheIllusion24 <ggfekarc@gmail.com>
2024-07-15 08:54:31 +0800

    Revert "test: disable rpm-ostree-automatic.timer"
    This reverts commit f47ed4c989988a16e6822a3e06a318cc5c2f5d21.

--- scripts/systemd/disable-auto-update.sh
+++ scripts/systemd/disable-auto-update.sh
@@ -2,7 +2,6 @@
 
 set -ouex pipefail
 
-systemctl disable rpm-ostreed-automatic.timer \
-    flatpak-system-update.timer
+systemctl disable flatpak-system-update.timer
 
 systemctl --global disable flatpak-user-update.timer
