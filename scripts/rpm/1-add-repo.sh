a2347666e929dd96987f85afcf350d9fb642dcd5
TheIllusion24 <ggfekarc@gmail.com>
2024-07-27 16:16:56 +0800

    chore: remove hardened-chromium

--- scripts/rpm/1-add-repo.sh
+++ scripts/rpm/1-add-repo.sh
@@ -4,4 +4,3 @@ set -euox pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-curl -Lo /etc/yum.repos.d/secureblue-hardened-chromium-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/secureblue/hardened-chromium/repo/fedora-"${RELEASE}"/secureblue-hardened-chromium-fedora-"${RELEASE}".repo
--- scripts/rpm/3-install-rpm.sh
+++ scripts/rpm/3-install-rpm.sh
@@ -5,7 +5,6 @@ set -ouex pipefail
 rpm-ostree install \
     firewall-config \
     gnome-themes-extra \
-    hardened-chromium \
     lm_sensors \
     setools \
     setroubleshoot \
