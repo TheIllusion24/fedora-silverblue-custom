8843c01f47407ffc68f66c5b6cd366ede2478584
TheIllusion24 <ggfekarc@gmail.com>
2024-07-27 11:22:14 +0800

    chore: update scripts/rpm/1-add-repo.sh

--- scripts/rpm/1-add-repo.sh
+++ scripts/rpm/1-add-repo.sh
@@ -4,4 +4,4 @@ set -euox pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-curl -Lo https://copr.fedorainfracloud.org/coprs/secureblue/hardened-chromium/repo/fedora-"${RELEASE}"/secureblue-hardened-chromium-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/secureblue-hardened-chromium-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/secureblue/hardened-chromium/repo/fedora-"${RELEASE}"/secureblue-hardened-chromium-fedora-"${RELEASE}".repo
